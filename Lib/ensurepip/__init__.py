47f5298e91cdfa13bf3c56045d5c7dc2493ff1c7
Miro Hrončok <miro@hroncok.cz>
2020-02-11 18:08:11 +0100

    WIP: Make ensurepip work with py3-only wheels

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -12,13 +12,17 @@ __all__ = ["version", "bootstrap"]
 
 _WHEEL_DIR = "/usr/share/python-wheels/"
 
+_wheels = {}
 
 def _get_most_recent_wheel_version(pkg):
     prefix = os.path.join(_WHEEL_DIR, "{}-".format(pkg))
-    suffix = "-py2.py3-none-any.whl"
-    pattern = "{}*{}".format(prefix, suffix)
-    versions = (p[len(prefix):-len(suffix)] for p in glob.glob(pattern))
-    return str(max(versions, key=distutils.version.LooseVersion))
+    _wheels[pkg] = {}
+    for suffix in "-py2.py3-none-any.whl", "-py3-none-any.whl":
+        pattern = "{}*{}".format(prefix, suffix)
+        for path in glob.glob(pattern):
+            version_str = path[len(prefix):-len(suffix)]
+            _wheels[pkg][version_str] = os.path.basename(path)
+    return str(max(_wheels[pkg], key=distutils.version.LooseVersion))
 
 
 _SETUPTOOLS_VERSION = _get_most_recent_wheel_version("setuptools")
@@ -108,7 +112,7 @@ def _bootstrap(*, root=None, upgrade=False, user=False,
         # additional paths that need added to sys.path
         additional_paths = []
         for project, version in _PROJECTS:
-            wheel_name = "{}-{}-py2.py3-none-any.whl".format(project, version)
+            wheel_name = _wheels[project][version]
             with open(os.path.join(_WHEEL_DIR, wheel_name), "rb") as sfp:
                 with open(os.path.join(tmpdir, wheel_name), "wb") as fp:
                     fp.write(sfp.read())
